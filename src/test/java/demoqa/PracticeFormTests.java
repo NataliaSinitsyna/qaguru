2ec28e5cfdaffcfdae4bd8ad63f8cc63439fb5c0
NataliaSinitsyna <natalia.a.sinitsyna@gmail.com>
2024-10-11 15:18:15 +0300

    update class PracticeFormTests

--- src/test/java/demoqa/PracticeFormTests.java
+++ src/test/java/demoqa/PracticeFormTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -17,7 +18,7 @@ public class PracticeFormTests {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true; //false
+        Configuration.holdBrowserOpen = false;
     }
 
     @Test
@@ -30,12 +31,15 @@ public class PracticeFormTests {
         $("#userEmail").setValue("alla.ivanova@test.com");
         $(byText("Female")).click();
         $("#userNumber").setValue("89001231223");
-        //date
-        //subj
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").click();
+        $(byText("February")).click();
+        $(".react-datepicker__year-select").click();
+        $(byText("2000")).click();
+        $(byText("18")).click();
         $("#subjectsContainer").click();
-        $(".subjects-auto-complete__control.subjects-auto-complete__control--is-focused.subjects-auto-complete__control--menu-is-open.css-1pahdxg-control").setValue("p");
+        $("#subjectsInput").setValue("p");
         $(byText("Physics")).click();
-
         $(byText("Reading")).click();
         $(byText("Sports")).click();
         File photo = new File("src/test/resources/photo.jpg");
@@ -46,6 +50,10 @@ public class PracticeFormTests {
         $("#city").click();
         $(byText("Delhi")).click();
         $("#submit").click();
+
+        $(".table-responsive").shouldHave(text("Alla Ivanova"), text("alla.ivanova@test.com"),
+                text("Female"), text("8900123122"), text("18 February,2000"), text("Physics"), text("Reading, Sports"),
+                text("photo.jpg"), text("Moscow"), text("NCR Delhi"));
     }
 }
 
